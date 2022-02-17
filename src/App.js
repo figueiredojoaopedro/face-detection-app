eabfa003d6e32f31e02edc6e97c869c35970c6d3
figueiredojoaopedro <neewprojecti@gmail.com>
2022-02-17 15:22:51 -0300

    onInputChange and OnButtonChange done

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import Rank from "./components/rank/Rank";
 import "./App.css";
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input: '',
+        }
+    }
+    onInputCHange = (event) => {
+        console.log(event.target.value);
+    }
+    onButtonSubmit = () => {
+        console.log('click');
+    }
     render() {
         return (
             <div className="App">
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm 
+                    onInputCHange={this.onInputCHange} 
+                    onButtonSubmit={this.onButtonSubmit}
+                />
                 {/*<FaceRecognition/>*/}
             </div>
         );

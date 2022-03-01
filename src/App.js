f7cefdf06148735eab6d6d95b722f64b6d3209e7
figueiredojoaopedro <neewprojecti@gmail.com>
2022-03-01 16:05:10 -0300

    adding the clarifai api

--- src/App.js
+++ src/App.js
@@ -5,14 +5,50 @@ import ImageLinkForm from "./components/imagelinkform/ImageLinkForm.js";
 import Rank from "./components/rank/Rank";
 import "./App.css";
 
+// to require the clarifai api
+import Clarifai from 'clarifai';
+
+// to access the api that im using...
+const app = new Clarifai.App({
+    apiKey: "36a126e3ad854417b93b7167e0384ab1",
+});
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input: ''
+        }
+    }
+    // as we are creating something that changes the dom, we've got to accept an event at the function
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+    // to catch the detect click
+    onButtonSubmit = () => {
+        setImageUrl(input)
+        app.models.predict(
+            Clarifai.FACE_DETECT_MODEL,
+        )
+        .then((response) => {
+            console.log(
+                response.outputs[0].data.regions[0].region_info.bounding_box
+            );
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm 
+                    onSubmit={this.onButtonSubmit} 
+                    onInputChange={this.onInputChange}   
+                />
                 {/*<FaceRecognition/>*/}
             </div>
         );
--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './imageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div className="ma4 mt0">
             <p className="f3 white">
@@ -11,8 +11,8 @@ const ImageLinkForm = () => {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input className="f4 pa2 w-70 center" type={"text"}></input>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+                    <input className="f4 pa2 w-70 center" type={"text"} onChange={onInputChange}></input>
+                    <button onClick={onButtonSubmit} className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
                         Detect
                     </button>
                 </div>
